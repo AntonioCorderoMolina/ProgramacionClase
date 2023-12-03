f16781252628360ed7c569bd7243d75985d55bad
Antonio Cordero Molina <acormol2001@g.educaand.es>
2023-12-04 00:42:02 +0100

    Actualización 03/12/23

--- tema5/EjercicioClaseExponenteNegativo.java
+++ tema5/EjercicioClaseExponenteNegativo.java
@@ -6,31 +6,59 @@ public class EjercicioClaseExponenteNegativo {
     
     public static void main(String[] args) {
 
-        int base;
-        int exponente;
-        int cont = 0;
-        double resultado = 0;
-        double divisor;
-        
+        int numero;
+        int posicion;
+        int divisor;
+
+        int parte1;
+        int parte2;
+
+        int i;
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("\n Introducza la base: ");
-        base = sc.nextInt();
-        System.out.print(" Introducza el exponente negativo: ");
-        exponente = sc.nextInt();
+        System.out.print("\n Introduce un número entero positivo: ");
+        numero = sc.nextInt();
 
-        exponente = Math.abs(exponente);
-
-        divisor = base;
-        
-        while (cont <= exponente) {
-            resultado = 1 / divisor; 
-            divisor = divisor * base;
-            cont ++;
+    // El número tiene 2 digitos??
+        if (numero < 10) {
+            System.out.println(" El número debe tener al menos dos dígitos.");
+            sc.close();
+            return;
         }
 
-        System.out.printf(" %d^-%d = %f\n", base, exponente, resultado);
+        System.out.print(" Introduce la posición a partir de la cual quieres partir el número: ");
+        posicion = sc.nextInt();
+
+    // Calcular la longitud del número
+        int longitud = contarDigitos(numero);
+
+    // Calcular las dos partes del número
+        divisor = 1;
+
+        for ( i = 1; i < (longitud - posicion + 1); i++) {
+            divisor *= 10;
+        }
+
+        parte1 = numero / divisor;
+        parte2 = numero % divisor;
+
+        System.out.printf(" Los números partidos son el %d y el %d.\n", parte1, parte2);
 
     sc.close();
+    }
+    
+    /**
+     * Función para contar el número de dígitos en un número
+     * @param num
+     * @return
+     */
+    private static int contarDigitos(int num) {
+        int cont = 0;
+        while (num != 0) {
+            num /= 10;
+            cont++;
+        }
+        return cont;
     }
 }
